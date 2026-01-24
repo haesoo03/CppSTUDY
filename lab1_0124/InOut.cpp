51ece680116a282c75991b9a109cb5fbcaaad627
Haesoo Keem <lkhskhs@naver.com>
2026-01-24 20:47:45 +0900

    study about Output, using namespace

---
+++ lab1_0124/InOut.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    
+    //1.basic
+    //std::cout << "Hello World!" << std::endl;
+
+    //2.namespace
+    using namespace std;
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
